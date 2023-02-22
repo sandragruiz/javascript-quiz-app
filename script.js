e6fd4b38e995d36cbc071cd7ed7b875c0a9e2515
unknown <sandra.gutierrez.ru@gmail.com>
2023-02-22 16:46:37 -0600

    added js pseudocode, commented html and css

---
+++ script.js
@@ -0,0 +1,51 @@
+//Pseudocode
+
+// criteria: when I click the start button, a timer starts and I am presented with a question//
+
+    // Establish questions
+    //1. Commonly used data types do NOT include:
+        //strings
+        //booleans
+        //alerts
+        //numbers
+    //2. The condition in an if/else statement is enclosed with:
+        //quotes
+        //curly brackets
+        //parenthesis
+        //square brackets
+    //3. Arrays in JavaScript can be used to store:
+        //numbers and strings
+        //other arrays
+        //booleans
+        //all of the above
+    //4. String values must be enclosed within ____ when being assigned to variables.
+        //commas
+        //curly brackets 
+        //quotes
+        //parenthesis
+    //5. A very useful tool used during development and debugging for printing content to the debugger is:
+        //JavaScript
+        //terminal/bash
+        //for loops
+        //console log
+
+    //set timer function
+    //set events for clicking the start button (starts the countdown and presents the first question)
+
+// criteria: when I answer a question, then I am presented with another question//
+
+    //set event for clicking an answer button that leads to next question
+
+// criteria: when I answer incorrectly, time (10 sec) is subtracted from the timer //
+
+    // establish which answers are correct and incorrect
+    // when clicking on incorrect answer, time must be reduced by 10 seconds
+
+// when all questions are answered or timer reaches 0, the game is over
+    
+    //set event for when timer ends or finish questions, the end screen appears
+    //end screen is currently under class quiz-end
+
+// when game is over, I can save my initials and score
+
+    //
